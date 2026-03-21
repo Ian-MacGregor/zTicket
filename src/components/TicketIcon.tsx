b87bb39d2d4c65b670363833e77cf9002813d3e6
Ian-MacGregor <ian@zarkapl.com>
2026-03-21 10:45:45 -0400

    Icon tweaks

--- src/components/TicketIcon.tsx
+++ src/components/TicketIcon.tsx
@@ -18,6 +18,16 @@ export default function TicketIcon({ size = 24, className = "" }: TicketIconProp
     >
       {/* Ticket outline with semicircular notches on left and right sides */}
       <path d="M3 5 L21 5 Q23 5 23 7 L23 9.5 A2.5 2.5 0 0 0 23 14.5 L23 17 Q23 19 21 19 L3 19 Q1 19 1 17 L1 14.5 A2.5 2.5 0 0 0 1 9.5 L1 7 Q1 5 3 5 Z" />
+      <text
+        x="12"
+        y="14.5"
+        textAnchor="middle"
+        fontSize="8"
+        fontWeight="700"
+        fontFamily="sans-serif"
+        fill="currentColor"
+        stroke="none"
+      >z</text>
     </svg>
   );
 }
